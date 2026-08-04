17b6a78651731abb49ee34de69602b0da597dd32
Raind <127611616+Juemuren@users.noreply.github.com>
2026-08-05 07:55:22 +0800

    style: 编写 ResourceCard 组件

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,21 @@
-import { docsLoader } from "@astrojs/starlight/loaders";
-import { docsSchema } from "@astrojs/starlight/schema";
+import { docsLoader, i18nLoader } from "@astrojs/starlight/loaders";
+import { docsSchema, i18nSchema } from "@astrojs/starlight/schema";
+import { z } from "astro/zod";
 import { defineCollection } from "astro:content";
 
 export const collections = {
-  docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
+  docs: defineCollection({
+    loader: docsLoader(),
+    schema: docsSchema(),
+  }),
+  i18n: defineCollection({
+    loader: i18nLoader(),
+    schema: i18nSchema({
+      extend: z.object({
+        "resourceCard.book": z.string(),
+        "resourceCard.video": z.string(),
+        "resourceCard.web": z.string(),
+      }),
+    }),
+  }),
 };
