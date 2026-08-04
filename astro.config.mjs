38b8751e86e52ea46f7f633cb2169cf40eadf10c
Raind <127611616+Juemuren@users.noreply.github.com>
2026-08-04 23:26:58 +0800

    config: 配置 jq & ignore 的语法高亮
    - jq 语法文件来自 https://github.com/wader/vscode-jq/blob/master/syntaxes/jq.tmLanguage.json
    - ignore 语法文件来自 https://github.com/dunstontc/vscode-gitignore-syntax/blob/master/syntaxes/gitignore.tmLanguage.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,26 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
-import mermaid from 'astro-mermaid';
+import mermaid from "astro-mermaid";
+
+import { jq, ignore } from "./src/syntaxes/index.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://juemuren.github.io',
-  base: 'notes',
+  site: "https://juemuren.github.io",
+  base: "notes",
   integrations: [
     mermaid(),
     starlight({
+      expressiveCode: {
+        shiki: {
+          langs: [jq, ignore],
+        },
+      },
       locales: {
         root: {
-          label: '简体中文',
-          lang: 'zh-CN',
+          label: "简体中文",
+          lang: "zh-CN",
         },
       },
       title: "笔记",
---
+++ src/syntaxes/index.mjs
@@ -0,0 +1,13 @@
+import { readFileSync } from "node:fs";
+
+function getGrammarJson(lang) {
+  return JSON.parse(
+    readFileSync(
+      new URL(`./${lang}.tmLanguage.json`, import.meta.url),
+      "utf-8",
+    ),
+  );
+}
+
+export const jq = getGrammarJson("jq")
+export const ignore = getGrammarJson("ignore");
