f2fdf5ddab723d36aedb666c154e76d44574112a
Raind <127611616+Juemuren@users.noreply.github.com>
2026-08-04 21:28:27 +0800

    config: 配置 Mermaid

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
+import mermaid from 'astro-mermaid';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://juemuren.github.io',
   base: 'notes',
   integrations: [
+    mermaid(),
     starlight({
       locales: {
         root: {
