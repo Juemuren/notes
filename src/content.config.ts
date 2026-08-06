5ea8aacab8e4bcc1a83af01dd3dc13fb28e2fbde
Raind <127611616+Juemuren@users.noreply.github.com>
2026-08-06 18:05:14 +0800

    style: 简化 ResourceCard 的设计
    - 去除 meta 部分冗余的文字

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,6 +1,5 @@
 import { docsLoader, i18nLoader } from "@astrojs/starlight/loaders";
 import { docsSchema, i18nSchema } from "@astrojs/starlight/schema";
-import { z } from "astro/zod";
 import { defineCollection } from "astro:content";
 
 export const collections = {
@@ -10,12 +9,6 @@ export const collections = {
   }),
   i18n: defineCollection({
     loader: i18nLoader(),
-    schema: i18nSchema({
-      extend: z.object({
-        "resourceCard.book": z.string(),
-        "resourceCard.video": z.string(),
-        "resourceCard.web": z.string(),
-      }),
-    }),
+    schema: i18nSchema(),
   }),
 };
